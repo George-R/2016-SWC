c61661647d76c27d85cc3a6ecda14339c3c868bc
George-R <george.redak91@gmail.com>
2016-10-04 13:58:05 -0400

    Assignment 3

---
+++ ps-03/squares.js
@@ -0,0 +1,10 @@
+
+
+
+  var canvas = document.getElementById('canvas');
+  var drawingPad = canvas.getContext('2d');
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(0, 0, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(250, 250, 300, 300);
