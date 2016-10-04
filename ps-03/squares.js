92da1f11489382f9c2c7596d430511681de53638
George-R <george.redak91@gmail.com>
2016-10-04 15:34:55 -0400

    Different result

--- ps-03/squares.js
+++ ps-03/squares.js
@@ -7,4 +7,37 @@
   drawingPad.fillRect(0, 0, 250, 250);
 
   drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-  drawingPad.fillRect(250, 250, 300, 300);
+  drawingPad.fillRect(0, 500, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(500, 0, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(250, 250, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(250, 750, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(500, 500, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(750, 750, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(750, 250, 250, 250);
+
+  drawingPad.fillStyle = "hsla(0,0%,100%,1)";
+  drawingPad.fillRect(425, 425, 150, 150);
+
+  drawingPad.fillStyle = "hsla(0,0%,100%,1)";
+  drawingPad.fillRect(175, 175, 150, 150);
+
+  drawingPad.fillStyle = "hsla(0,0%,100%,1)";
+  drawingPad.fillRect(675, 675, 150, 150);
+
+  drawingPad.fillStyle = "hsla(0,0%,100%,1)";
+  drawingPad.fillRect(175, 675, 150, 150);
+
+  drawingPad.fillStyle = "hsla(0,0%,100%,1)";
+  drawingPad.fillRect(675, 175, 150, 150);
