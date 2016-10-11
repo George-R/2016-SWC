1e94fd4238688a2b0a9a1136528abd7c50ff3560
George-R <george.redak91@gmail.com>
2016-10-11 18:20:50 -0400

    33 lines changes

---
+++ ps-04/33.js
@@ -0,0 +1,36 @@
+var myCanvas = document.getElementById('canvas');
+ var drawingPad = myCanvas.getContext('2d');
+
+ // @params:
+ // hue -- number 0-360 (think color wheel degrees)
+ // saturation number 0-100
+ // lightness  number 0-100  0 == maximum-dark, 100 == maximum-light
+ // alpha  number 0-1  0 == transparent, 0.5 == half opaque, 1 == solid
+ var getColor = function(hue, saturation, lightness, alpha) {
+   var colorString = "hsla(" +
+     hue + ", "
+     + saturation + "%,"
+     + lightness  + "%,"
+     + alpha + ")";
+   return colorString;
+ };
+
+ var drawSquare = function(x,y,size) {
+   drawingPad.fillRect(x * size, y * size, size , size);
+   drawingPad.strokeStyle = getColor(0, 0, 100, 0.3);
+   drawingPad.strokeRect(x * size, y * size/size, size, size);
+ }
+
+ var size = 15;
+ var numX = 500;
+ var numY = 500;
+ var Lightness = 0;
+
+ for(var x = 0 ; x < numX; x++) {
+   for(var y = 0 ; y < numY; y++) {
+     Lightness = x;
+     drawingPad.fillStyle = getColor(0, 0, Lightness, 1);
+     drawSquare(x,y,size);
+
+   }
+ }
